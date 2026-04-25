f91067c4c2a3a7255c94d900aa693c304b55b505
Hacker <romanrus98@mail.ru>
2026-04-25 13:58:35 +0500

    block user

--- api/routes/admin.php
+++ api/routes/admin.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\Admin\PostController;
 use App\Http\Controllers\Admin\UserController;
 
-Route::middleware(['auth:sanctum', 'role:admin|super-admin|editor|user'])->group(function () {
+Route::middleware(['auth:sanctum', 'role:admin|super-admin|editor'])->group(function () {
     Route::apiResource('users', UserController::class);
     Route::apiResource('posts', PostController::class);
 });
