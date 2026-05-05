b34564f2b94306d331f82b68f00e0cee5a50320d
Hacker <romanrus98@mail.ru>
2026-05-05 22:33:25 +0500

    add confirmed

--- admin/app/resources/user.ts
+++ admin/app/resources/user.ts
@@ -1,6 +1,6 @@
 import { z } from 'zod';
 import { resourceSchema } from './resource';
-import {roleSchema} from "~/resources/role";
+import { Roles, roleSchema } from "~/resources/role";
 
 export const userSchema = resourceSchema.extend({
     name: z.string(),
@@ -15,18 +15,48 @@ export const loginSchema = z.object({
     password: z.string().min(8, 'Password must be at least 8 characters.')
 })
 
-export const updateUserSchema = z.object({
+const userFormBaseSchema = z.object({
     name: z.string().min(1, 'Name is required.'),
     email: z.email('Please enter a valid email address.'),
-    password: z
-        .string()
-        .min(8, 'Password must be at least 8 characters.')
-        .optional()
-        .or(z.literal(''))
+    role: roleSchema,
 })
 
-export type LoginCredentials = z.infer<typeof loginSchema>;
-export type UpdateUserCredentials = z.infer<typeof updateUserSchema>;
+export const createUserSchema = userFormBaseSchema.extend({
+    password: z.string().min(8, 'Password must be at least 8 characters.'),
+    password_confirmation: z.string().min(8, 'Please confirm password.'),
+}).refine((data) => data.password === data.password_confirmation, {
+    message: 'Passwords do not match.',
+    path: ['password_confirmation'],
+})
+
+export const updateUserSchema = userFormBaseSchema
+    .extend({
+        password: z
+            .string()
+            .optional()
+            .or(z.literal('')),
+
+        password_confirmation: z
+            .string()
+            .optional()
+            .or(z.literal('')),
+    })
+    .refine((data) => {
+        if (!data.password) return true
+
+        return data.password.length >= 8
+    }, {
+        message: 'Password must be at least 8 characters.',
+        path: ['password'],
+    })
+    .refine((data) => {
+        if (!data.password) return true
+
+        return data.password === data.password_confirmation
+    }, {
+        message: 'Passwords do not match.',
+        path: ['password_confirmation'],
+    })
 
 export const tokenSchema = z.object({
     access_token: z.string(),
@@ -35,3 +65,6 @@ export const tokenSchema = z.object({
 })
 
 export type TokenResource = z.infer<typeof tokenSchema>;
+export type CreateUserCredentials = z.infer<typeof createUserSchema>;
+export type LoginCredentials = z.infer<typeof loginSchema>;
+export type UpdateUserCredentials = z.infer<typeof updateUserSchema>;
