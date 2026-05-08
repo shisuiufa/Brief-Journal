cd0d5987026fead13a77ecb1d91624f9acdad673
Hacker <romanrus98@mail.ru>
2026-05-09 01:16:03 +0500

    utils

---
+++ web/src/utils/formatDate.ts
@@ -0,0 +1,20 @@
+const articleDateFormatter = new Intl.DateTimeFormat('en-US', {
+  month: 'short',
+  day: 'numeric',
+  year: 'numeric',
+  timeZone: 'UTC',
+})
+
+export const formatDate = (value: string | number | Date | null | undefined): string => {
+  if (!value) {
+    return ''
+  }
+
+  const date = value instanceof Date ? value : new Date(value)
+
+  if (Number.isNaN(date.getTime())) {
+    return ''
+  }
+
+  return articleDateFormatter.format(date)
+}
