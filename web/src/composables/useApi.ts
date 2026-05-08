24cf734b7f27473ee81c5aada6ecb7e97684bf96
Hacker <romanrus98@mail.ru>
2026-05-09 01:16:57 +0500

    api

--- web/src/composables/useApi.ts
+++ web/src/composables/useApi.ts
@@ -1,13 +1,12 @@
 import { apiKey } from '@/plugins/api'
-import type { ApiRequestOptions } from '@/types/api'
 import { inject } from 'vue'
 
-export const useApi = <TResponse>(request: string, options?: ApiRequestOptions) => {
+export const useApi = () => {
   const api = inject(apiKey)
 
   if (!api) {
     throw new Error('API plugin is not installed')
   }
 
-  return api<TResponse>(request, options)
+  return api
 }
